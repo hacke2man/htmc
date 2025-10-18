4a1f52b59a310064557a15982014580995096c2d
liam <hacke2man@proton.me>
2025-10-17 21:58:44 -0700

    switch example to serve-static

--- main.js
+++ main.js
@@ -1,11 +1,3 @@
-fs = require('node:fs');
-index = fs.readFileSync('./static/usage.html').toString();
-htmc = fs.readFileSync('./static/htmc.js').toString();
-htmc_min = fs.readFileSync('./static/htmc.min.js').toString();
-component = fs.readFileSync('./static/foo.html').toString();
 require('polka')()
-	.get('/htmc.js', (req,res)=>res.end(htmc))
-	.get('/htmc.min.js', (req,res)=>res.end(htmc_min))
-	.get('/foo.html', (req,res)=>res.end(component))
-	.get('/', (req,res)=>res.end(index))
+	.use(require('serve-static')('./public'))
 	.listen(3000, _=>console.log(`running localhost:3000`));
